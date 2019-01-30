22634448a1238e90709341e818082130b35fdcec
eyael <eyaelja1@gmail.com>
2019-01-30 14:19:51 -0500

    first commit

---
+++ AvarageScore1/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class AvarageScore1   {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Test/src/com/company/Test.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Test {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Test");
+    }
+}
